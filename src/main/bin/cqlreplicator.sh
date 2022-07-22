e65007accb01bd1505c00b374d85e75285e48f43
Gaurav Sharma <zgaurash@amazon.com>
2022-07-22 17:18:39 +0000

    Update cqlreplicator.sh

--- src/main/bin/cqlreplicator.sh
+++ src/main/bin/cqlreplicator.sh
@@ -2,6 +2,9 @@
 #Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #SPDX-License-Identifier: Apache-2.0
 
+# If using AWS SMPS to store credentials, uncomment the following line:
+#export AWS_SMPS_MODE=true
+
 #basefolder=$(pwd -L)
 #export CQLREPLICATOR_HOME=${basefolder:0:${#basefolder}-4}
 export CQLREPLICATOR_CONF=$CQLREPLICATOR_HOME"/conf"
@@ -27,6 +30,8 @@ aws s3 cp s3://"$BUCKETNAME"/"$KEYSPACENAME"/"$TABLENAME"/KeyspacesConnector.con
 
 # Updating credentials for Cassandra and Keyspaces from Parameter Store (If using other credential manager, update accordingly)
 
+if [ -n "${AWS_SMPS_MODE}" ]; then
+
 USERNAME1=$(grep 'username' $CQLREPLICATOR_HOME/conf/KeyspacesConnector.conf | cut -d '=' -f2)
 PASSWORD1=$(aws ssm get-parameter --name $USERNAME1 --with-decryption | jq '.Parameter.Value')
 sed -i '' -e "s#password = .*#password = $PASSWORD1#" $CQLREPLICATOR_HOME/conf/KeyspacesConnector.conf
@@ -34,6 +39,8 @@ sed -i '' -e "s#password = .*#password = $PASSWORD1#" $CQLREPLICATOR_HOME/conf/K
 USERNAME2=$(grep 'username' $CQLREPLICATOR_HOME/conf/CassandraConnector.conf | cut -d '=' -f2)
 PASSWORD2=$(aws ssm get-parameter --name $USERNAME2 --with-decryption | jq '.Parameter.Value')
 sed -i '' -e "s#password = .*#password = $PASSWORD2#" $CQLREPLICATOR_HOME/conf/CassandraConnector.conf
+
+fi 
 
 if [ -n "${BUCKETNAME}" ] && [ -n "${KEYSPACENAME}" ] && [ -n "${TABLENAME}" ]; then
   if [ -z "${DEV_MODE}" ]; then

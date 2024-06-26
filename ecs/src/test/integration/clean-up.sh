fd4688238da4f8aa739ddf231fe1decd975afaee
michaelraney <raneymike83@gmail.com>
2024-06-25 21:13:57 -0400

    deprecate ecs cqlreplicator
    deprecate ecs cqlreplicator

--- ecs/src/test/integration/clean-up.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#
-# // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# // SPDX-License-Identifier: Apache-2.0
-#
-# Cleanup
-kill $(jps -ml | grep com.amazon.aws.cqlreplicator.Starter | awk '{print $1}')
-docker container stop test-cql-replicator
-docker container rm test-cql-replicator
